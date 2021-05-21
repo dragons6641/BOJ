15286c5a95166c429eac6b6496248112be3ab828
dragons6641 <43373202+dragons6641@users.noreply.github.com>
2021-05-21 19:12:24 +0900

    랜덤 게임~
    https://www.acmicpc.net/problem/10943

---
+++ extra/BOJ10943.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <ctime>
+
+#define MIN_NUM 1
+#define MAX_NUM 10
+
+using namespace std;
+
+int main(void)
+{
+	// srand((unsigned int)time(0));
+
+	// cout << ((rand() % MAX_NUM) + MIN_NUM) << endl;
+
+	cout << 7 << endl;
+
+	return 0;
+}
