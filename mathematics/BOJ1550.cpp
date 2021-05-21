ae958f4f7c0260b1543c5d6957ed971e85584ab3
dragons6641 <43373202+dragons6641@users.noreply.github.com>
2021-05-21 16:30:55 +0900

    16진수
    https://www.acmicpc.net/problem/1550

---
+++ mathematics/BOJ1550.cpp
@@ -0,0 +1,20 @@
+/*
+1550 16진수
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define MAX_LENGTH 6
+
+int main(void)
+{
+	int N;
+
+	scanf("%x", &N);
+
+	printf("%d\n", N);
+
+	return 0;
+}
