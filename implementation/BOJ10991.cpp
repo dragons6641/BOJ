9f3a5e49c8a8ed35ade4c00be11d15d282aaf1cd
dragons6641 <43373202+dragons6641@users.noreply.github.com>
2021-05-21 19:18:12 +0900

    별 찍기 - 16
    https://www.acmicpc.net/problem/10991

---
+++ implementation/BOJ10991.cpp
@@ -0,0 +1,31 @@
+/*
+10991 별 찍기 - 16
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+
+	scanf("%d", &N);
+
+	for (register int i = 1; i <= N; i++)
+	{
+		for (register int j = i + 1; j <= N; j++)
+		{
+			printf(" ");
+		}
+
+		for (register int j = 1; j <= i; j++)
+		{
+			printf("* ");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
