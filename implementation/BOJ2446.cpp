faa16462ef339515d8902ac86fb1395ec13ca3be
dragons6641 <43373202+dragons6641@users.noreply.github.com>
2021-05-21 17:09:10 +0900

    별 찍기 - 9
    https://www.acmicpc.net/problem/2446

---
+++ implementation/BOJ2446.cpp
@@ -0,0 +1,57 @@
+/*
+2446 별 찍기 - 9
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+
+	scanf("%d", &N);
+
+	for (register int i = 1; i < N; i++)
+	{
+		for (register int j = 1; j < i; j++)
+		{
+			printf(" ");
+		}
+
+		printf("*");
+
+		for (register int j = i; j < N; j++)
+		{
+			printf("**");
+		}
+
+		printf("\n");
+	}
+
+	for (register int j = 1; j < N; j++)
+	{
+		printf(" ");
+	}
+
+	printf("*\n");
+
+	for (register int i = 1; i < N; i++)
+	{
+		for (register int j = i + 1; j < N; j++)
+		{
+			printf(" ");
+		}
+
+		printf("*");
+
+		for (register int j = 1; j <= i; j++)
+		{
+			printf("**");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
