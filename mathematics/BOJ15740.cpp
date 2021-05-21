91d733c7822edf7c0085a7dc8ff252ee7501fd6c
dragons6641 <43373202+dragons6641@users.noreply.github.com>
2021-05-21 20:11:41 +0900

    A+B - 9
    https://www.acmicpc.net/problem/15740

---
+++ mathematics/BOJ15740.cpp
@@ -0,0 +1,323 @@
+/*
+15740 A+B - 9
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define ll long long
+
+#include <stdio.h>
+
+#define MAX_LENGTH 10000
+#define DIGIT 10
+
+int main(void)
+{
+	int lenA = 0;
+	int lenB = 0;
+	int cmp = 0;
+	register int i = 0;
+	bool flag = true;
+	bool isPositiveA = true;
+	bool isPositiveB = true;
+
+	char A[MAX_LENGTH + 10] = { 0, };
+	char B[MAX_LENGTH + 10] = { 0, };
+	char carry[MAX_LENGTH + 10] = { 0, };
+	char ans[MAX_LENGTH + 10] = { 0, };
+
+	for (i = 0; i <= MAX_LENGTH + 8; i++)
+	{
+		carry[i] = '0';
+	}
+
+	scanf("%s %s", &A, &B);
+
+	if ((A[0] == '0') && (B[0] == '0'))
+	{
+		printf("0\n");
+
+		return 0;
+	}
+
+	i = 0;
+
+	while (A[i] || B[i])
+	{
+		if (A[i])
+		{
+			lenA++;
+		}
+
+		if (B[i])
+		{
+			lenB++;
+		}
+
+		i++;
+	}
+
+
+	for (i = 0; i < (lenA / 2); i++)
+	{
+		char tmp = A[i];
+		A[i] = A[lenA - i - 1];
+		A[lenA - i - 1] = tmp;
+	}
+
+	for (i = 0; i < (lenB / 2); i++)
+	{
+		char tmp = B[i];
+		B[i] = B[lenB - i - 1];
+		B[lenB - i - 1] = tmp;
+	}
+
+	if (A[lenA - 1] == '-')
+	{
+		A[lenA - 1] = '\0';
+		isPositiveA = false;
+		lenA--;
+	}
+
+	if (B[lenB - 1] == '-')
+	{
+		B[lenB - 1] = '\0';
+		isPositiveB = false;
+		lenB--;
+	}
+
+	i = 0;
+
+	if (isPositiveA ^ isPositiveB)
+	{
+		if (lenA != lenB)
+		{
+			cmp = lenA - lenB;
+		}
+		else
+		{
+			register int j = lenA - 1;
+
+			while (j >= 0)
+			{
+				if (A[j] != B[j])
+				{
+					cmp = A[j] - B[j];
+
+					break;
+				}
+
+				j--;
+			}
+		}
+
+		if (cmp > 0)
+		{
+			while (A[i] || B[i])
+			{
+				int tmp;
+
+				if (A[i] && B[i])
+				{
+					tmp = A[i] - B[i];
+
+					if (tmp < 0)
+					{
+						ans[i] = (tmp + DIGIT) + '0';
+						A[i + 1]--;
+					}
+					else
+					{
+						ans[i] = tmp + '0';
+					}
+				}
+				else if (A[i])
+				{
+					if (A[i] < '0')
+					{
+						ans[i] = A[i] + DIGIT;
+						A[i + 1]--;
+					}
+					else
+					{
+						ans[i] = A[i];
+					}
+				}
+				else if (B[i])
+				{
+					if (B[i] < '0')
+					{
+						ans[i] = B[i] + DIGIT;
+						B[i + 1]--;
+					}
+					else
+					{
+						ans[i] = B[i];
+					}
+				}
+
+				i++;
+			}
+
+			i--;
+
+			if (!isPositiveA && isPositiveB)
+			{
+				ans[++i] = '-';
+			}
+		}
+		else if (cmp < 0)
+		{
+			while (B[i] || A[i])
+			{
+				int tmp;
+
+				if (B[i] && A[i])
+				{
+					tmp = B[i] - A[i];
+
+					if (tmp < 0)
+					{
+						ans[i] = (tmp + DIGIT) + '0';
+						B[i + 1]--;
+					}
+					else
+					{
+						ans[i] = tmp + '0';
+					}
+				}
+				else if (B[i])
+				{
+					if (B[i] < '0')
+					{
+						ans[i] = B[i] + DIGIT;
+						B[i + 1]--;
+					}
+					else
+					{
+						ans[i] = B[i];
+					}
+				}
+				else if (A[i])
+				{
+					if (A[i] < '0')
+					{
+						ans[i] = A[i] + DIGIT;
+						A[i + 1]--;
+					}
+					else
+					{
+						ans[i] = A[i];
+					}
+				}
+
+				i++;
+			}
+
+			i--;
+
+			if (!isPositiveB && isPositiveA)
+			{
+				ans[++i] = '-';
+			}
+		}
+		else
+		{
+			printf("0\n");
+
+			return 0;
+		}
+	}
+	else
+	{
+		while (A[i] || B[i])
+		{
+			int tmp;
+			char next;
+
+			if (A[i] && B[i])
+			{
+				tmp = A[i] + B[i] + carry[i] - ('0' * 3);
+
+				if (tmp >= DIGIT)
+				{
+					ans[i] = (tmp - DIGIT) + '0';
+					carry[i + 1] = '1';
+				}
+				else
+				{
+					ans[i] = tmp + '0';
+				}
+			}
+			else if (A[i])
+			{
+				next = A[i] + carry[i] - '0';
+
+				if (next >= DIGIT + '0')
+				{
+					ans[i] = next - DIGIT;
+					carry[i + 1] = '1';
+				}
+				else
+				{
+					ans[i] = next;
+				}
+			}
+			else if (B[i])
+			{
+				next = B[i] + carry[i] - '0';
+
+				if (next >= DIGIT + '0')
+				{
+					ans[i] = next - DIGIT;
+					carry[i + 1] = '1';
+				}
+				else
+				{
+					ans[i] = next;
+				}
+			}
+
+			i++;
+		}
+
+		if (carry[i] == '0')
+		{
+			i--;
+		}
+		else
+		{
+			ans[i] = carry[i];
+		}
+
+		if ((!isPositiveA) && (!isPositiveB))
+		{
+			ans[++i] = '-';
+		}
+	}
+
+	if (ans[i] == '-')
+	{
+		printf("%c", ans[i--]);
+	}
+
+	for (register int j = i; j >= 0; j--)
+	{
+		if (flag)
+		{
+			if (ans[j] == '0')
+			{
+				continue;
+			}
+			else
+			{
+				flag = false;
+			}
+		}
+
+		printf("%c", ans[j]);
+	}
+
+	printf("\n");
+
+	return 0;
+}
