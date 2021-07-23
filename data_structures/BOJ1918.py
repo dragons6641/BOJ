c6da17e77f783ed5cf434bc2353a503650710498
J058_김진옥 <43373202+dragons6641@users.noreply.github.com>
2021-07-23 14:08:17 +0900

    후위 표기식
    https://www.acmicpc.net/problem/1918

---
+++ data_structures/BOJ1918.py
@@ -0,0 +1,35 @@
+import sys;
+
+OPERATOR_DICT = {'+' : 0, '-' : 0, '*' : 1, '/' : 1};
+
+# sys.stdin = open("input1.txt", 'r');    # ABC+*
+# sys.stdin = open("input2.txt", 'r');    # AB*C*D*
+# sys.stdin = open("input3.txt", 'r');    # AB+C+D+
+# sys.stdin = open("input4.txt", 'r');    # ABC*DE-G**+
+
+(infixStr, postfixStr) = (input(), "");
+
+stack = [];
+
+for curCh in infixStr:
+    if (curCh == '('):
+        stack.append(curCh);
+    elif (curCh == ')'):
+        while (stack[-1] != '('):
+            postfixStr += stack.pop();
+
+        stack.pop();
+    elif (curCh in OPERATOR_DICT):
+        while ((stack) and (stack[-1] != '(') and (OPERATOR_DICT[curCh] <= OPERATOR_DICT[stack[-1]])):
+            postfixStr += stack.pop();
+
+        stack.append(curCh);
+    else:
+        postfixStr += curCh;
+
+    # print(curCh, stack);
+
+while (stack):
+    postfixStr += stack.pop();
+
+print(postfixStr);
