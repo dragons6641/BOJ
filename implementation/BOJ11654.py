0a70e641d4fd3ad573cb8715fdd96c3b012323e9
J058_김진옥 <43373202+dragons6641@users.noreply.github.com>
2021-07-26 11:52:40 +0900

    아스키 코드
    https://www.acmicpc.net/problem/11654

---
+++ implementation/BOJ11654.py
@@ -0,0 +1,10 @@
+import sys;
+
+# sys.stdin = open("input1.txt", 'r');    # 65
+# sys.stdin = open("input2.txt", 'r');    # 67
+# sys.stdin = open("input3.txt", 'r');    # 48
+# sys.stdin = open("input4.txt", 'r');    # 57
+# sys.stdin = open("input5.txt", 'r');    # 97
+# sys.stdin = open("input6.txt", 'r');    # 122
+
+print(ord(input()));
